521b26b38410dc34d745157c0ab649d4993ac0b2
YongsinLim <micky.lys@gmail.com>
2026-03-17 10:48:26 +0800

    N_Queen - add note

--- Level_2/N_queens/queen.h
+++ Level_2/N_queens/queen.h
@@ -6,7 +6,7 @@
 /*   By: yolim <yolim@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 20:03:37 by yolim             #+#    #+#             */
-/*   Updated: 2026/03/14 20:05:46 by yolim            ###   ########.fr       */
+/*   Updated: 2026/03/16 23:03:32 by yolim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 
 #include <stdio.h> // for fprintf
 #include <stdlib.h> // for atoi, calloc, free, abs
+#include <unistd.h> // for write
 
-int ft_abs(int x);
-int is_safe(int *board, int col, int row, int n);
+int     ft_abs(int x);
+int     is_safe(int *board, int col, int row, int n);
 void   solve_n_queens(int *board, int col, int n);
 
 #endif
